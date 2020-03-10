a90dc610c48eff96c643d43b0d192aafab1e52be
Aaron Maurais <mauraisa@bc.edu>
2020-03-10 16:04:16 -0400

    Add optional positional args for input directories.

--- dtsetup.sh
+++ dtsetup.sh
@@ -19,13 +19,31 @@ while ! [[ -z "$1" ]] ; do
 			exit
 		;;
 		*)
-			echo "$1" $invalidOptionMessage
-			usage
-			exit
-		;;
+            while ! [[ -z "$1" ]] ; do
+                if [[ "$1" == -* ]] ; then
+                   usage
+                else
+                    posArgs+=( "$1" )
+                    destDir="$temp"
+                fi
+                shift
+            done
+        ;;
 	esac
 	shift
 done
+
+inDirs=()
+#if no posArgs were given, use current working dir
+if [ ${#posArgs[@]} -eq 0 ]; then
+    for d in ./* ; do
+        inDirs+=( $d )
+    done
+else
+    for ((i=0;i<${#posArgs[@]};++i)); do
+        inDirs+=( ${posArgs[i]} )
+    done
+fi
 
 #genearte a folder in parent dir with the name $newDirName and copy all DTA-filter
 #files into $nweDirName using specified inputFormat
@@ -37,7 +55,7 @@ case $inputFormat in
 	"std")
 		#itterate through all dirs one level below parent dir looking for
 		#DTA_filter files
-		for D in ./* ; do
+        for D in ${inDirs[@]} ; do
 			#skip new dir just made
 			if ! [ "${D/dtarraySetup}" = "$D" ] ; then
 				continue;
@@ -47,8 +65,9 @@ case $inputFormat in
 			if [ -d "$D" ] ; then
 				cd $D
 				if [ -f DTASelect-filter.txt ] ; then
-					echo -e "\tAdding $D..."
-					cp DTASelect-filter.txt ../$newDirName/"$D".dtafilter
+                    base_temp=$(basename "$D")
+					echo -e "\tAdding $base_temp..."
+					cp DTASelect-filter.txt ../$newDirName/"$base_temp".dtafilter
 				fi
 				cd ..
 			fi
@@ -56,7 +75,7 @@ case $inputFormat in
 	;;
 	"subdir")
         # Find all DTAFilter files one level below current directory.
-		for f in $(find *|grep 'DTASelect-filter.txt$') ; do
+		for f in $(printf '%s\n' "${inDirs[@]}" | xargs find|grep 'DTASelect-filter.txt$') ; do
             newBase=$(dirname "$f"| sed 's/\//_/g')
             cp -v "$f" $newDirName/"$newBase".dtafilter
 		done
